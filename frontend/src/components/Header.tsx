032ce79d48e825ae39c28ba101b919ade008d0df
DemetrPI <90642921+DemetrPI@users.noreply.github.com>
2023-10-25 21:35:00 +0200

    Add main component files

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,27 @@
+import { AppBar, Toolbar } from "@mui/material";
+
+const Header = () => {
+  return (
+    <AppBar
+      sx={{
+        bgcolor: "transparent",
+        position: "sticky",
+        boxShadow: "lg",
+
+      }}
+      
+    >
+<Toolbar
+sx= {{
+    display: "flex",
+    
+}}
+>
+
+</Toolbar>
+
+    </AppBar>
+  );
+};
+
+export default Header;
