2975674350f29e94669c1eb7fb60b7538587387b
DemetrPI <90642921+DemetrPI@users.noreply.github.com>
2023-10-28 14:40:28 +0200

    Add images styling auth component

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,24 +1,22 @@
 import { AppBar, Toolbar } from "@mui/material";
+import Logo from "./shared/Logo";
 
 const Header = () => {
   return (
     <AppBar
       sx={{
         bgcolor: "transparent",
-        position: "sticky",
+        position: "static",
         boxShadow: "lg",
-
       }}
-      
     >
-<Toolbar
-sx= {{
-    display: "flex",
-    
-}}
->
-
-</Toolbar>
+      <Toolbar
+        sx={{
+          display: "flex",
+        }}
+      >
+        <Logo/>
+      </Toolbar>
 
     </AppBar>
   );
