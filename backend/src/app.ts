0995623542e9f314c0f3991cb4e46026923ff282
DemetrPI <90642921+DemetrPI@users.noreply.github.com>
2023-11-03 20:36:27 +0100

    Fix logout bug (NavLink Props onclick)

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,7 +2,6 @@ import express from "express";
 import { config } from "dotenv";
 import morgan from "morgan";
 import appRouter from "./routes/index.js";
-// import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import path from "path";
@@ -11,17 +10,17 @@ config();
 const app = express();
 
 //middlewares
-app.use(cors({origin:"http://localhost:5173", credentials:true}));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
-app.use(express.static('./client'))
-app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "index.html"))
-})
+// app.use(express.static('./client'))
+// app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "index.html"))
+// })
 
 // //logs for dev only
-// app.use(morgan("dev"));
+app.use(morgan("dev"));
 
 app.use("/api/v1", appRouter);
 
--- backend/src/utils/constants.ts
+++ backend/src/utils/constants.ts
@@ -1,2 +1,2 @@
-export const COOKIE_NAME = "auth_token"
-export const DOMAIN ="https://mern-chat.azurewebsites.net"
+export const COOKIE_NAME = "auth_token";
+export const DOMAIN ="https://mern-chat.azurewebsites.net";
