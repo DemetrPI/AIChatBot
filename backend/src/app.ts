76e456644281b377de03569c57f94070c6fff4a6
DemetrPI <90642921+DemetrPI@users.noreply.github.com>
2023-11-03 20:39:39 +0100

    Prepare build mode

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { config } from "dotenv";
-import morgan from "morgan";
+// import morgan from "morgan";
 import appRouter from "./routes/index.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
@@ -14,13 +14,13 @@ app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
-// app.use(express.static('./client'))
-// app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "index.html"))
-// })
+app.use(express.static('./client'))
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "index.html"))
+})
 
-// //logs for dev only
-app.use(morgan("dev"));
+// // //logs for dev only
+// app.use(morgan("dev"));
 
 app.use("/api/v1", appRouter);
 
