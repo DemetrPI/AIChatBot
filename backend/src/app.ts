8536c52ec6466c7d4b33ac76eb92cf50d5d84d21
DemetrPI <90642921+DemetrPI@users.noreply.github.com>
2023-11-05 14:14:31 +0100

    Update URls for production

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -16,9 +16,13 @@ config();
 const app = express();
 
 //middlewares
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://green-field-04240d803.4.azurestaticapps.net/", credentials: true }));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
+
+app.use("/api/v1", appRouter);
+
+export default app;
 
 // app.use(express.static('./public'))
 // app.get("*", (req, res) => {
@@ -27,7 +31,3 @@ app.use(cookieParser(process.env.COOKIE_SECRET));
 
 // // //logs for dev only
 // app.use(morgan("dev"));
-
-app.use("/api/v1", appRouter);
-
-export default app;
