af0092ce24303ea5b4fd21b736725d453734d08f
DemetrPI <90642921+DemetrPI@users.noreply.github.com>
2023-11-06 19:52:11 +0100

    Prepare for production

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -3,12 +3,20 @@ import { config } from "dotenv";
 import appRouter from "./routes/index.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 config();
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 //middlewares
-// app.use(cors({ origin: "https://green-field-04240d803.4.azurestaticapps.net/", credentials: true }));
+app.use(express.static('../dist/client'));
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../dist/client", "index.html"));
+});
 app.use(cors({
-    origin: ["https://green-field-04240d803.4.azurestaticapps.net", "http://localhost:5173"],
+    origin: ["https://green-field-04240d803.4.azurestaticapps.net", "http://localhost:5173", "https://mern-chat.azurewebsites.net"],
     credentials: true,
     optionsSuccessStatus: 204,
     exposedHeaders: ["set-cookie"]
