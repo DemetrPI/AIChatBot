3631cca0aa9e1e284f8558f8138ddd41a398968d
Dmytro Pishchenkov <90642921+DemetrPI@users.noreply.github.com>
2023-11-07 15:10:17 +0100

    Update app.ts (copy app.js)

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,31 +6,31 @@ import cors from "cors";
 import path from "path";
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-
 config();
-
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 //middlewares
+// Serve the /assets route with the express.static middleware
+app.use('/assets', express.static(path.join(__dirname, 'dist', 'client', 'assets')));
 
-app.use(express.static('../dist/client'))
-app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../dist/client", "index.html"))
-})
+// Handle other routes and serve index.html for all unmatched routes
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'dist', 'client', 'index.html'), {
+    headers: {
+      'Content-Type': 'text/html',
+    },
+  });
+});
 
-app.use(
-  cors({
-  origin: ["https://green-field-04240d803.4.azurestaticapps.net","http://localhost:5173", "https://mern-chat.azurewebsites.net" ],
-    credentials: true, // Set to true if you're using cookies or sessions
+app.use(cors({
+    origin: ["https://green-field-04240d803.4.azurestaticapps.net", "http://localhost:5173", "https://mern-chat.azurewebsites.net"],
+    credentials: true,
     optionsSuccessStatus: 204,
     exposedHeaders: ["set-cookie"]
-  })
-);
-
+}));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use("/api/v1", appRouter);
-
 export default app;
