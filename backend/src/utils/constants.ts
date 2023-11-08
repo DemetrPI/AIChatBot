1387b5c98443235458c415030bf4d67fddfbf1fa
Dmytro Pishchenkov <demetriy.78@gmail.com>
2023-11-08 16:52:35 +0100

    updated domain for frontend

--- backend/src/utils/constants.ts
+++ backend/src/utils/constants.ts
@@ -1,3 +1,4 @@
 export const COOKIE_NAME = "auth_token";
-export const DOMAIN = "http://localhost:5000/";
+// export const DOMAIN = "http://localhost:5000/";
+export const DOMAIN = "https://green-field-04240d803.4.azurestaticapps.net/";
 
