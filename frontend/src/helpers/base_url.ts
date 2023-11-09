fe7acaafbab74ba38bb767d4f6f074f3830409c2
DemetrPI <90642921+DemetrPI@users.noreply.github.com>
2023-11-09 19:26:59 +0100

    Update baseURL for Versel

--- frontend/src/helpers/base_url.ts
+++ frontend/src/helpers/base_url.ts
@@ -1,1 +1,2 @@
-export const BASE_URL ="https://mern-chat.azurewebsites.net/api/v1"
+// export const BASE_URL ="https://mern-chat.azurewebsites.net/api/v1"
+export const BASE_URL ="https://ai-chat-bot-backend.vercel.app/api/v1"
