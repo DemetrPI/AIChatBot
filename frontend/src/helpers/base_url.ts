200991e417cb7899198b4a0d09b077dbaf33a97f
Dmytro Pishchenkov <demetriy.78@gmail.com>
2023-11-10 11:32:12 +0100

    Removed port from base URL

--- frontend/src/helpers/base_url.ts
+++ frontend/src/helpers/base_url.ts
@@ -1,3 +1,3 @@
 // export const BASE_URL ="https://mern-chat.azurewebsites.net/api/v1"
 // export const BASE_URL ="https://ai-chat-bot-backend.vercel.app/api/v1"
-export const BASE_URL ="https://chat-ai-backend-ehk6.onrender.com:5000/api/v1"
+export const BASE_URL ="https://chat-ai-backend-ehk6.onrender.com/api/v1/"
