47f79da865f124643b83f6de380241d0bad43546
Dmytro Pishchenkov <demetriy.78@gmail.com>
2023-11-10 12:21:37 +0100

    Rebuild backend

--- backend/dist/utils/constants.js
+++ backend/dist/utils/constants.js
@@ -1,5 +1,4 @@
 export const COOKIE_NAME = "auth_token";
 //  export const DOMAIN = "localhost";
-// export const DOMAIN = "ai-chat-bot-delta.vercel.app/"
-export const DOMAIN = "ai-chat-1mwy.onrender.com/";
+export const DOMAIN = ".onrender.com";
 //# sourceMappingURL=constants.js.map
