6031aef44bfadab6d27c0cabfaf02d8fddb84f07
Dmytro Pishchenkov <demetriy.78@gmail.com>
2023-11-10 11:43:40 +0100

    Update DOMAIN in backend

--- backend/src/utils/constants.ts
+++ backend/src/utils/constants.ts
@@ -1,4 +1,3 @@
 export const COOKIE_NAME = "auth_token";
 //  export const DOMAIN = "localhost";
-// export const DOMAIN = "ai-chat-bot-delta.vercel.app/"
-export const DOMAIN = "ai-chat-1mwy.onrender.com/"
+export const DOMAIN = "ai-chat-1mwy.onrender.com"
